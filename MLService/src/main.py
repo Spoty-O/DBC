31bcea9c209789732e53beb27b2b47f0df41e524
Spoty-O <korotysh16@ukr.net>
2025-07-06 00:24:02 +0300

    Check signing key

--- MLService/src/main.py
+++ MLService/src/main.py
@@ -8,13 +8,13 @@ import uvicorn
 
 
 async def main():
+    # app = FastAPI()
+    #     env: EnvironmentConfig = get_env()
+    #     uvicorn.run(app=app, port=env.ML_PORT, reload=env.isDevMode)
     for e in DataScraperService().getFilesData():
         FileManagerService().save("stash.txt", e)
 
 
-# app = FastAPI()
 
 if __name__ == "__main__":
-    #     env: EnvironmentConfig = get_env()
-    #     uvicorn.run(app=app, port=env.ML_PORT, reload=env.isDevMode)
     asyncio.run(main())
